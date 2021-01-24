40874246d22f6e2991e15c20b3fd46bfe365faa8
aichiroudeduorou <1906829481@qq.com>
2021-01-24 16:04:40 +0800

    添加项目文件。

---
+++ experiment2.cpp
@@ -0,0 +1,24 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 
+
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "QUEUE.h"
+using namespace std;
+
+extern const char* TestQUEUE(int& s);
+
+int main() {
+    int s;
+    const char* e = TestQUEUE(s);
+    cout << s << endl;
+    cout << e << endl;
+    getchar();
+    return 0;
+}
+
+
+
+
+
